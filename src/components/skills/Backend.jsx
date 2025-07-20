e9ee2eec865d4493b9171070e680846f77ba5f0d
Megh <meghd9@gmail.com>
2025-07-20 12:53:06 +0100

    Changes to hero section

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -24,7 +24,7 @@ const Backend = () => {
                 <div className="skills__data">
                     <HiCheckBadge />
                     <div>
-                        <h3 className="skills__name">MySQL</h3>
+                        <h3 className="skills__name">MongoDB</h3>
                         {/* <span className="skills__level">Basic</span> */}
                     </div>
                 </div>
